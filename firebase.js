5f6ae46893e4c3c8724bb85e297d11b8828d116e
ShindroS <stracinskydodo@gmail.com>
2026-02-13 14:29:03 +0100

    firebase fix

--- firebase.js
+++ firebase.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBtl2Eqn1apZdt2usKnK27LTu2PFt5ffNo",
   authDomain: "articlegame.firebaseapp.com",
@@ -14,5 +9,6 @@ const firebaseConfig = {
   appId: "1:1073793185399:web:db30ade86a924878173872"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase (compat SDK loaded via <script> tags in HTML)
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
