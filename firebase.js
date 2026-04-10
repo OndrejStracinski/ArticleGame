7c393732e36480aaa30ff22198c452257c6ac7b0
ShindroS <stracinskydodo@gmail.com>
2026-04-10 08:49:53 +0200

    questions and firebase

--- firebase.js
+++ firebase.js
@@ -1,4 +1,9 @@
-// Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBtl2Eqn1apZdt2usKnK27LTu2PFt5ffNo",
   authDomain: "articlegame.firebaseapp.com",
@@ -6,9 +11,8 @@ const firebaseConfig = {
   projectId: "articlegame",
   storageBucket: "articlegame.firebasestorage.app",
   messagingSenderId: "1073793185399",
-  appId: "1:1073793185399:web:db30ade86a924878173872"
+  appId: "1:1073793185399:web:74ec25dbe28e51e2173872"
 };
 
-// Initialize Firebase (compat SDK loaded via <script> tags in HTML)
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
